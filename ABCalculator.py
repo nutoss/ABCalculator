dbd11d5b0d1c5d1631826e451d676a9d9fd5277c
nutoss <113785849+nutoss@users.noreply.github.com>
2022-12-01 16:18:01 +0300

    Добавление файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+#AB калькулятор 
+
+import tkinter as tk
+
+#Создание главного окна
+root=tk.Tk()
+root.geometry("280x300")
+root.title ("A/B калькулятор")
+
+#Запуск цикла mainloop
+root.mainloop()
