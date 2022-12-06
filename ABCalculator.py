ef7defd2edae607bbe198cbd5b4805c903b86f80
nutoss <113785849+nutoss@users.noreply.github.com>
2022-12-06 16:23:02 +0300

    Добавление значений в поля ввода

--- ABCalculator.py
+++ ABCalculator.py
@@ -32,33 +32,36 @@ lbtTitle1.place(x=25, y=55)
 lblVisitors1 = tk.Label(text = "Посетители", font = ('Helvetica',10, 'bold'))
 lblVisitors1.place(x=25, y=85)
 
-entVisitors1 = tk.Entry(font = ('Helvetica',10, 'bold'))
+
+entVisitors1 = tk.Entry(font = ('Helvetica',10, 'bold'), justify = 'center') #justify вырав по центру
 entVisitors1.place(x=115, y=85, width=90, height=20)
+entVisitors1.insert(tk.END, '0')
 
 lblConversions1 = tk.Label(text = "Конверсии", font = ('Helvetica',10, 'bold'))
 lblConversions1.place(x=25, y=115)
 
-entConversions1 = tk.Entry(font = ('Helvetica',10, 'bold'))
+entConversions1 = tk.Entry(font = ('Helvetica',10, 'bold'), justify = 'center')
 entConversions1.place(x=115, y=115, width=90, height=20)
-
+entConversions1.insert(tk.END, '0')
 
 #Добавление метки заголовка тестовой группы
-lbtTitle2 = tk.Label(text = "Тестовая группа", font = ('Helvetica',12, 'bold'), fg = '#0066ff')
+lbtTitle2 = tk.Label(text = "Тестовая группа", font = ('Helvetica',12, 'bold'), fg = '#008800')
 lbtTitle2.place(x=25, y=145)
 
 #Добавление полей ввода контрольной группы
 lblVisitors2 = tk.Label(text = "Посетители", font = ('Helvetica',10, 'bold'))
 lblVisitors2.place(x=25, y=175)
 
-entVisitors2 = tk.Entry(font = ('Helvetica',10, 'bold'))
+entVisitors2 = tk.Entry(font = ('Helvetica',10, 'bold'), justify = 'center')
 entVisitors2.place(x=115, y=175, width=90, height=20)
+entVisitors2.insert(tk.END, '0')
 
 lblConversions2 = tk.Label(text = "Конверсии", font = ('Helvetica',10, 'bold'))
 lblConversions2.place(x=25, y=205)
 
-entConversions2 = tk.Entry(font = ('Helvetica',10, 'bold'))
+entConversions2 = tk.Entry(font = ('Helvetica',10, 'bold'), justify = 'center')
 entConversions2.place(x=115, y=205, width=90, height=20)
-
+entConversions2.insert(tk.END, '0')
 
 
 #Добавление кнопки Рассчитать
